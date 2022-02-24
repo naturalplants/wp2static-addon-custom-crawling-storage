6113dcc072a93e5b40483252ac8f481563035bca
ohayn <59242656+ohayn@users.noreply.github.com>
2022-02-24 19:01:57 +0900

    Update src/CLI.php

--- src/CLI.php
+++ src/CLI.php
@@ -14,25 +14,5 @@ use WP_CLI;
  */
 class CLI {
 
-    /**
-     * S3 commands
-     *
-     * @param string[] $args CLI args
-     * @param string[] $assoc_args CLI args
-     */
-    public static function s3(
-        array $args,
-        array $assoc_args
-    ) : void {
-        $action = isset( $args[0] ) ? $args[0] : null;
-
-        if ( empty( $action ) ) {
-            WP_CLI::error( 'Missing required argument: <options>' );
-        }
-
-        if ( $action === 'options' ) {
-            WP_CLI::line( 'TBC setting options for S3 addon' );
-        }
-    }
 }
 
